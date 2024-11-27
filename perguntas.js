6abd4f99a302ce99b49866bffe8b62fae23b896a
vinicuralesbica <martinsjau.com@gmail.com>
2024-11-27 10:55:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália',
+    'A capital da Austrália é Sydney'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'eu te amo em ingles é I Love You (AI LÓVE IU)'
+)
